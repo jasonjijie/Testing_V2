17489d6f84637ae055dcf42cbdbbf134df0f1e53
jasonjijie <47234288+jasonjijie@users.noreply.github.com>
2019-12-06 11:21:07 -0800

    Update apply().R

--- apply().R
+++ apply().R
@@ -1,67 +1,92 @@
-#################
-#apply()        #
-#lapply()       # 
-#sapply()       #
-#tapply()       #
-#################
+#####################
+#apply()            #
+#lapply()           # 
+#sapply()           #
+#tapply()           #
+#####################
 
-
-
-
-## 1. apply(): apply(x,margin,fun)
-
-m1 <- matrix(c(1:10), nrow=5,ncol=6)
+#apply() function:
+# 1 is row, 2 is column
+m1 <- matrix(c(1:10), nrow=5, ncol=6)
 a_m1 <- apply(m1, 2,sum)
 
 
+l1 <- c(4,9,6)
+l2 <- c(25,36,49)
+matrix1 <- cbind(l1,l2)
+
+matrix1_apply1 <- apply(matrix1, 1, sum)
+
+# a little vit diff, apply() take 1st column in matrix, then return 1st row, then take second column in matrix return second row
+matrix1_apply2 <- apply(matrix1, 1, sqrt)
+
+#take 1st column, return first column, then take second column, return second column
+matrix1_apply3 <- apply(matrix1, 2, sqrt)
+
+
+# 1) customer you function
+myfunction <- function(x){
+  result <- 1+2+sqrt(x)
+  return(result)
+}
+
+matrix1_apply21 <- apply(matrix1, 1, function(x) myfunction(x))
+matrix1_apply31 <- apply(matrix1, 2, function(x) myfunction(x))
+
+# 2)
+myfunction2 <- function(x,y){
+  result <- sqrt(x)+y
+  return(result)
+}
+
+matrix1_apply221 <- apply(matrix1, 1, function(x) myfunction2(x,3))
+
+
+# 3) 
+  myfunction2 <- function(x,y){
+result <- sqrt(x)+y
+  return(result)
+  }
+  
+# this tell you matrix value which corresponsing to y value, not x anymore  
+matrix1_apply221 <- apply(matrix1, 1, function(x) myfunction2(3,y))
+  
+#Conclusion: apply() use function to target matrix
 
 
 
 
-## 2. lapply(): Return a list.
+#sapply: return a integer: "double"
+matrix1_sapply <- sapply(matrix1, sqrt)
+
+
+
+#lapply(): return a independence lists:
+
+matrix1_lapply <- lapply(matrix1, sqrt)
+typeof(matrix1_lapply)
+
 
 movies <- c("SPYDERMAN","BATMAN","VERTIGO","CHINATOWN")
-movies_lower <- lapply(movies,tolower)
-str(movies_lower)
+movies_lower <- lapply(movies, tolower)
 
 
 
 
 
+#do call(): use do call get a biger matrix
+t2 <- do.call(rbind, lapply(matrix1, sqrt))
+t21 <- do.call(cbind, lapply(matrix1,sqrt))
 
-## 3, sapply(): Return a vector
-dt <- cars
-smn_cars <- sapply(dt, min)
-
-
-
-
-
-
-## 4. Difference between sapply() & lapply(): Because max() function doesn't apply to a list but only to a vector.
-
-l <- list("a", "b", c("x", "y"), c("1","3", "4"))
-sz <- max(sapply(l, function(x) length(x)))
-
-sz <- max(lapply(l, function(x) length(x)))
+#we also can design a data frame:
+t4 <- data.frame(lapply(matrix1, sqrt))
 
 
 
 
+                          
 
-
-## 5. We can built in fun(), and used it inside the apply & sapply()
-avg <- function(x){
-  ((min(x)+max(x))/2)
-}
-fun_cars <- sapply(dt, avg)
-
-
-
-
-
-
-## 6. tapply(): tapply(Summary variable, Group variable, Function)
+# tapply(): tapply(Summary variable, Group variable, Function)
 gl('number of levels', '# replication', "Length of results")
 medical_example <- data.frame(patietID=1:100, age=rnorm(100, mean=60, sd=12), 
                               treatment=gl(2,50), labels=c("Treatment","Control"))
